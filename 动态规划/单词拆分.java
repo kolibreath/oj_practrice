17912d58b124b112310cc4d0664485819cd4197e
kolibreath <734780178@qq.com>
2021-05-22 11:51:24 +0800

    [ADD] 5.22

--- 动态规划/单词拆分.java
+++ 动态规划/单词拆分.java
@@ -1,7 +1,16 @@
 public class 单词拆分 {
     class Solution {
         public boolean wordBreak(String s, List<String> wordDict) {
-
+            boolean dp[] = new boolean[wordDict.size()+1];
+            dp[0] = true;
+            for( int i = 1; i <= wordDict.size(); i++){
+                for(int j = 0; j < i; j++){
+                    String sub = s.substring(j,i);
+                    if(wordDict.contains(sub) && dp[j]) 
+                        dp[i] = dp[j];
+                }
+            }
+            return dp[wordDict.size()];
         }
     }
 }
